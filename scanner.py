030c5f69aa9c5721ecd6d343397b339ce14e9bcc
Faissal <faissal.lauar@aurea.com>
2024-08-28 18:43:05 -0300

    scanner code clean

--- scanner.py
+++ scanner.py
@@ -20,37 +20,44 @@ Please enter the type of scan you want to run:
 
 print("You have selected option: ", resp)
 
-match resp:
-    case '1':   
-        print("Nmap version: ", scanner.nmap_version())
-        scanner.scan(ipAddr, "1-1024", "-v -sS", True)
-        print(scanner.scaninfo())
-        print("scanner keys: ", scanner.all_hosts())
-        print("scanner[ipAddr] keys: ", scanner[ipAddr].keys())
-        print("IP Status: ", scanner[ipAddr].state())
-        print(scanner[ipAddr].all_protocols())
-        print("Open Ports: ", scanner[ipAddr]['tcp'].keys())
+timeout = 30
 
-    case '2':   
-        print("Nmap version: ", scanner.nmap_version())
-        scanner.scan(ipAddr, "1-1024", "-v -sU", True)
-        print(scanner.scaninfo())
-        print("scanner keys: ", scanner.all_hosts())
-        print("scanner[ipAddr] keys: ", scanner[ipAddr].keys())
-        print("IP Status: ", scanner[ipAddr].state())
-        print(scanner[ipAddr].all_protocols())
-        print("Open Ports: ", scanner[ipAddr]['udp'].keys())
+try:
+    match resp:
+        case '1':   
+            scanner.scan(ipAddr, "1-1024", "-v -sS", True, timeout)
 
-    case '3':   
-        print("Nmap version: ", scanner.nmap_version())
-        scanner.scan(ipAddr, "1-1024", "-v -sS -sV -sC -A -O", True)
-        print(scanner.scaninfo())
-        print("scanner keys: ", scanner.all_hosts())
-        print("scanner[ipAddr] keys: ", scanner[ipAddr].keys())
-        print("IP Status: ", scanner[ipAddr].state())
-        print(scanner[ipAddr].all_protocols())
-        print("Open Ports: ", scanner[ipAddr]['tcp'].keys())
+        case '2':   
+            scanner.scan(ipAddr, "1-1024", "-v -sU", True, timeout)
 
-    case _:
-        print("Not a valid option.")
-        exit()
+        case '3':   
+            scanner.scan(ipAddr, "1-1024", "-v -sS -sV -sC -A -O", True, timeout)      
+
+        case _:
+            print("Not a valid option.")
+            exit(1)
+except nmap.nmap.PortScannerTimeout:
+    print("Scan timed out after ", timeout, " seconds")
+    exit(1)
+
+# if the user entered a hostname, get the resulting IP Address
+allHosts = scanner.all_hosts()
+ipAddr = allHosts[0] if len(allHosts) > 0 else None
+
+if not ipAddr:
+    print("The host name is not reachable")
+    exit(1)
+
+print("Nmap version: ", scanner.nmap_version())
+print(scanner.scaninfo())
+print("scanner keys: ", allHosts)
+print("scanner[ipAddr] keys: ", scanner[ipAddr].keys())
+print("IP Status: ", scanner[ipAddr].state())
+protocols = scanner[ipAddr].all_protocols()
+print("Protocols: ", protocols)
+
+if 'tcp' in protocols:
+    print("Open TCP Ports: ", scanner[ipAddr]['tcp'].keys())
+
+if 'udp' in protocols:
+    print("Open UDP Ports: ", scanner[ipAddr]['udp'].keys())
