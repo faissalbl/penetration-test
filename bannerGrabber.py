8bd77120d755c33118cacf5b0807469056c6d384
Faissal <faissal.lauar@aurea.com>
2024-08-28 16:36:07 -0300

    error handling

--- bannerGrabber.py
+++ bannerGrabber.py
@@ -2,18 +2,28 @@
 
 import socket
 
+defaultTimeout = 10
+
 def banner(ip, port):
-    s = socket.socket()
-    s.connect((ip, port))
-    buffer = 1024
-    result = s.recv(buffer).decode()
-    s.close()
+    result = ''
+    try:
+        s = socket.socket()
+        s.settimeout(defaultTimeout)
+        s.connect((ip, port))
+        buffer = 1024
+        result = s.recv(buffer).decode()
+        s.close()
+    except TimeoutError:
+        result = "Connection timed out. The default timeout is {} seconds".format(defaultTimeout)
     return result
 
 def main():
-    ip = input("Please enter the ip: ")
-    port = int(input("Please enter the port number: "))
-    print("grabbing banner for ip: {}, port: {}".format(ip, port))
-    print(banner(ip, port))
+    try:
+        ip = input("Please enter the ip: ")
+        port = int(input("Please enter the port number: "))
+        print("grabbing banner for ip: {}, port: {}".format(ip, port))
+        print(banner(ip, port))
+    except ValueError:
+        print("Invalid input. Please enter a valid ip and port")
 
 main()
